ea396c6d3ef2c3a3f17c8dc565d7210452b10831
Dhiraj Chawla <dchawla4@sapient.com>
2017-07-23 21:12:07 +0530

    Made post a new component

--- gql-client/src/app/post.component.ts
+++ gql-client/src/app/post.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, @Input } from '@angular/core';
 
 @Component({
+  selector: 'post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
 export class PostComponent {
-    constructor() {}
+  @Input() postObj:Object;
+  constructor() {}
+  onDelete(): void {
+
+  }
 }
